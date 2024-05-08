17b1892e996f9805f34e3941b67b1de30c5f9013
Obasan Tolulope <obasantolu@gmail.com>
2024-05-08 15:38:11 +0100

    created the appbar of the transaction screen

---
+++ lib/screens/transactions/components/app_bar.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TransAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const TransAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        onPressed: () {},
+        icon: Icon(
+          Icons.arrow_back,
+          size: 20,
+        ),
+      ),
+      title: Text(
+        'Transaction Statements',
+        style: TextStyle(
+          fontSize: 16.sp,
+          fontWeight: FontWeight.w500,
+          color: Color(0xff1B1C1E),
+        ),
+      ),
+      elevation: 0,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56);
+}
