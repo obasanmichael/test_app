f0e3a9e62dca321b826985d6cce56e96145e9787
Obasan Tolulope <obasantolu@gmail.com>
2024-05-08 15:39:26 +0100

    created the Successful Statement screen

---
+++ lib/screens/success_statement.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SuccessStatement extends StatelessWidget {
+  const SuccessStatement({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        
+      ),
+    );
+  }
+}
--- lib/screens/transactions/transaction_statement.dart
+++ lib/screens/transactions/transaction_statement.dart
@@ -1,25 +1,123 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:test_app/screens/transactions/components/app_bar.dart';
+import 'package:test_app/screens/transactions/components/filter_container.dart';
+import 'package:test_app/screens/transactions/components/format_container.dart';
 
-class TransactionStatement extends StatelessWidget {
+class TransactionStatement extends StatefulWidget {
   const TransactionStatement({super.key});
 
   @override
+  State<TransactionStatement> createState() => _TransactionStatementState();
+}
+
+class _TransactionStatementState extends State<TransactionStatement> {
+  Widget addHeight(double height) => SizedBox(height: height.h);
+
+  Widget addWidth(double width) => SizedBox(width: width.w);
+
+  void _openDurationLayer() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext ctx) => const FilterContainer(),
+    );
+  }
+
+  void _openFormatLayer() {
+    showModalBottomSheet(
+      
+      context: context,
+      builder: (context) => const FormatContainer(),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Padding(
-      padding: EdgeInsets.symmetric(vertical: 26.5.h, horizontal: 32.w),
-      child: Column(
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Icon(Icons.arrow_left),
-              Text('Transaction Statements', textAlign: TextAlign.center,)
-            ],
-          )
-        ],
+    return Scaffold(
+      appBar: TransAppBar(),
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 26.5.h, horizontal: 20.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Enter the necessary details',
+              style: TextStyle(
+                fontSize: 14.sp,
+                fontWeight: FontWeight.w300,
+                color: Color(0xff414244),
+              ),
+            ),
+            addHeight(24),
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xff8D8D8E),
+                  ),
+                  borderRadius: BorderRadius.circular(10.r),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xff8D8D8E),
+                  ),
+                  borderRadius: BorderRadius.circular(10.r),
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 16.w, vertical: 15.h),
+                suffixIcon: Icon(Icons.keyboard_arrow_down),
+                hintText: 'Format',
+              ),
+              onTap: _openDurationLayer,
+              readOnly: true,
+            ),
+            addHeight(16),
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xff8D8D8E),
+                  ),
+                  borderRadius: BorderRadius.circular(10.r),
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xff8D8D8E),
+                    ),
+                    borderRadius: BorderRadius.circular(10.r)),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 16.w, vertical: 15.h),
+                suffixIcon: Icon(Icons.keyboard_arrow_down),
+                hintText: 'Format',
+              ),
+              onTap: _openFormatLayer,
+              readOnly: true,
+            ),
+            addHeight(15),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 15.h),
+                  backgroundColor: Color(0xff4B0082),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.r),
+                  ),
+                ),
+                child: Text(
+                  'Generate',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
